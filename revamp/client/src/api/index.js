70a187f71ec453c91b8dc6b4a61a3bffce700ec6
aliadnan2000 <60284195+aliadnan2000@users.noreply.github.com>
2023-04-17 17:06:46 +0500

    deployment

--- revamp/client/src/api/index.js
+++ revamp/client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: 'http://localhost:5000'});
+const API = axios.create({baseURL: 'https://alumni-portal-backend-vwv0.onrender.com'});
 
 
 
